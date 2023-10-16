00a6466a77a564342757e0f0166ce87de6aa1da3
parvathysanthosh21 <parvathysanthosh231@gmail.com>
2023-10-16 12:28:06 +0530

    counterapp

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterSlice from "./counterSlice";
+
+
+export const store = configureStore({
+    reducer:{
+        // reducers should key-value pair
+        counter:counterSlice
+    }
+})
